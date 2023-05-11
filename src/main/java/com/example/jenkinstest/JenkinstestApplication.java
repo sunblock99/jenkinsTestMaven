6ddcb6730fc61abd7607ea98afd4545e1aa5cf46
선형채 <sunblock99@kakao.com>
2023-05-11 14:07:15 +0900

    테스트 코드 커밋

---
+++ src/main/java/com/example/jenkinstest/JenkinstestApplication.java
@@ -0,0 +1,13 @@
+package com.example.jenkinstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinstestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinstestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jenkinstest/JenkinstestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkinstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinstestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
