e9a1fbe30b07ec820641b18d73aca8e29a4b8de4
선형채 <sunblock99@kakao.com>
2023-05-23 14:51:32 +0900

    젠킨스 webhook test!!

--- src/main/java/com/example/jenkinstest/JenkinstestApplication.java
+++ src/main/java/com/example/jenkinstest/JenkinstestApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JenkinstestApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(JenkinstestApplication.class, args);
+        SpringApplication.run(JenkinstestApplication.class, args);   
     }
 
 }
